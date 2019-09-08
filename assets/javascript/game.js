3bc3dfc57861b8365194970db7d60ab58f4f4e2a
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-08 12:59:20 -0500

    Seems like it was an issue with how I was linking to game.js. I can get a console log to appear now. :)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
-    
-    var randomTarget;
-    function generateRandomTarget() {
-        randomTarget = Math.floor((Math.random * 120) + 19);
-        console.log(randomTarget);
-    }
+    console.log("hi");
 
 
 });
