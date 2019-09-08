1a574f457e77f2168bb63b0595837cd013a1fd54
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-08 17:27:20 -0500

    Got a random number generated and displaying, now need to limit it between 19-120.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    console.log("hi");
+    var targetNumber;
+    var targetNumberText;
+    $(function() {
+        // targetNumber = 15;
+        targetNumber = Math.floor((Math.random() * 120) + 19);
+        targetNumberText = targetNumber.toString();
+        console.log(targetNumberText);
+        $("#random-number").text(targetNumberText);
+    });
+
 
 
 });
