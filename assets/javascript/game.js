cdc037b79a78175784c92ae94e997fcae276b26c
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-08 21:10:54 -0500

    got gem values generating and gem 1 works on click and it's value is displayed.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,9 @@
 $(document).ready(function(){
     var targetNumber;
     var targetNumberText;
-    var baseGemValue;
-    var gem1Value;
-    var gem2Value;
-    var gem3Value;
-    var gem4Value; 
-    var gemValues = [
-        gem1Value,
-        gem2Value,
-        gem3Value,
-        gem4Value
-    ];
-    
+    var gemValues = [];
+    var currentScoreNumber = 0;
+
     // This sets the number we want to hit.
     function getTargetNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -21,13 +12,18 @@ $(document).ready(function(){
     targetNumberText = targetNumber.toString();
     $("#random-number").text(targetNumberText);
 
+    function getGemValue() {
+        for(var i = 0; i < 4; i++) {
+            gemValues[i] = (Math.floor((Math.random() * 12) + 1));         
+            console.log(gemValues);
+        }
+    }
 
+    getGemValue();
 
-    
-
-
-
-
-
-
+    $("#gem-1").click(function(){
+        currentScoreNumber += gemValues[0];
+        console.log(currentScoreNumber);
+        $("#currentScore").text(currentScoreNumber);
+    });
 });
