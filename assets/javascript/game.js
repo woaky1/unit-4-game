bafd647c012809be469932a9fcdf1323e545a89c
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-08 20:01:51 -0500

    Got the target number generating in the right range and displaying.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,32 @@
 $(document).ready(function(){
     var targetNumber;
     var targetNumberText;
-    $(function() {
-        // targetNumber = 15;
-        targetNumber = Math.floor((Math.random() * 120) + 19);
-        targetNumberText = targetNumber.toString();
-        console.log(targetNumberText);
-        $("#random-number").text(targetNumberText);
-    });
+    var baseGemValue;
+    var gem1Value;
+    var gem2Value;
+    var gem3Value;
+    var gem4Value; 
+    var gemValues = [
+        gem1Value,
+        gem2Value,
+        gem3Value,
+        gem4Value
+    ];
+    
+    // This sets the number we want to hit.
+    function getTargetNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+        }
+    targetNumber = getTargetNumber(19, 120);
+    targetNumberText = targetNumber.toString();
+    $("#random-number").text(targetNumberText);
+
+
+
+    
+
+
+
 
 
 
